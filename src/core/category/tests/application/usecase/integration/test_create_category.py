dbd27abcecc01b66b37030bb7131d3c65e93f78a
Leomara <leomara.andrade@gmail.com>
2025-12-28 09:29:00 -0300

    Ajustando estrutura de pasta

--- src/core/category/tests/application/usecase/integration/test_create_category.py
+++ src/core/category/tests/application/usecase/integration/test_create_category.py
@@ -1,11 +1,6 @@
-from unittest.mock import MagicMock
 from uuid import UUID
 
-import pytest
-
-from src.core.category.application.category_repository import CategoryRepository
-from src.core.category.application.create_category import CreateCategory, CreateCategoryRequest, CreateCategoryResponse
-from src.core.category.application.exceptions import InvalidCategoryData
+from src.core.category.application.usecase.create_category import CreateCategory, CreateCategoryRequest, CreateCategoryResponse
 from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
 
 
