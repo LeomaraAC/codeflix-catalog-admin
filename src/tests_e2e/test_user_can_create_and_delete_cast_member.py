ed3e711169b1d06b4baf545b61cc79a1c487357d
Leomara <leomara.andrade@gmail.com>
2026-03-29 17:23:40 -0300

    Test e2e cast member

---
+++ src/tests_e2e/test_user_can_create_and_delete_cast_member.py
@@ -0,0 +1,37 @@
+import pytest
+from rest_framework.test import APIClient
+
+
+@pytest.fixture
+def api_client() -> APIClient:
+    return APIClient()
+
+
+@pytest.fixture
+def base_url() -> str:
+    return '/api/cast_members/'
+
+
+@pytest.mark.django_db
+class TestCreateAndDeleteCastMember:
+    def test_user_can_create_and_delete_cast_member(self, api_client: APIClient, base_url: str) -> None:
+        list_response = api_client.get(base_url)
+        assert list_response.data == {'data': []}
+        cast_member_to_create = {'name': 'John Doe', 'type': 'DIRECTOR'}
+
+        # Cria um cast member
+        create_response = api_client.post(base_url, cast_member_to_create, format='json')
+
+        assert create_response.status_code == 201
+        created_cast_member_id = create_response.data['id']
+
+        assert api_client.get(base_url).data == {
+            'data': [{'id': created_cast_member_id, **cast_member_to_create}]
+        }
+
+        # Deleta cast member
+        delete_response = api_client.delete(f'{base_url}{created_cast_member_id}/')
+        assert delete_response.status_code == 204
+
+        # Verifica que a listagem está vazia novamente
+        assert api_client.get(base_url).data == {'data': []}
